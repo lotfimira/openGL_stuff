b042a9bdd1f7a5abc182d62d0de96628c2d509ef
Lotfi Mira <lotfim@mirageoscience.com>
2017-05-25 18:25:09 -0400

    Use TBN matrix to switch normals from tangent space into eye space

--- Grid_QT_GUI_VS2012/Shaders/blinn_phong_fragment.glsl
+++ Grid_QT_GUI_VS2012/Shaders/blinn_phong_fragment.glsl
@@ -3,7 +3,9 @@
 const float AMBIANT = 0.3;
 const float DIFFUSE = 0.8;
 
-in vec3 normal_eye;   // normal in eye space
+in vec3 normal_i;   // normal in eye space
+in vec3 tangent_i;
+in vec3 bitangent_i;
 in vec3 camera_vec_eye;
 in vec2 tex_coord_i;
 
@@ -17,15 +19,23 @@ uniform mat3 normal_mat;
 //-----------------------------------------------------------------------------
 vec4 BlinnPhongLighting(vec4 input_color)
 {
-    //init with Ambient Term:
+    // init with Ambient Term:
     vec4 frag_color = input_color * AMBIANT;
 
-    //add Diffuse Terms
     // normal in eye space
-    vec3 mapped_normal = texture(normal_texture, tex_coord_i).xyz;
-    mapped_normal = (mapped_normal - 0.5) * 2.0;
-    vec3 normal_eye = normalize(normal_mat * mapped_normal);
+    vec3 normal_tangent = texture(normal_texture, tex_coord_i).xyz;
+    normal_tangent = (normal_tangent - 0.5) * 2.0; // map to [-1; +1]
 
+    // up is Y so tangent space matrix should be TNB, not TBN
+    mat3 tnb_mat = mat3(tangent_i,
+                        normal_i,
+                        bitangent_i);
+
+    vec3 normal_world = tnb_mat * normal_tangent;
+    vec3 normal_eye = normal_mat * normal_world;
+    normal_eye = normalize(normal_eye);
+
+    // add diffuse term
     frag_color += input_color * DIFFUSE * max(dot(normal_eye, light_dir_eye), 0.0);
 
     // add specular term
--- Grid_QT_GUI_VS2012/Shaders/standard_vertex.glsl
+++ Grid_QT_GUI_VS2012/Shaders/standard_vertex.glsl
@@ -2,9 +2,15 @@
 
 in vec3 pos;
 in vec2 tex_coord;
+in vec3 normal;
+in vec3 tangent;
+in vec3 bitangent;
 
 out vec3 camera_vec_eye;
 out vec2 tex_coord_i;
+out vec3 normal_i;
+out vec3 tangent_i;
+out vec3 bitangent_i;
 
 uniform mat4 mvp_mat;
 uniform mat3 normal_mat;
@@ -18,4 +24,8 @@ void main(void)
     camera_vec_eye = normal_mat * (camera_pos - pos);
 
     tex_coord_i = tex_coord;
+
+    normal_i = normal;
+    tangent_i = tangent;
+    bitangent_i = bitangent;
 }
